93c72944d5c45ab3f662763dd6ebc82cee4dc744
Farkhod Allamuradov <farkhod5allamuradov@gmail.com>
2021-01-25 16:40:14 +0500

    Update coming soon js

--- pages/coming-soon.js
+++ pages/coming-soon.js
@@ -1,13 +1,13 @@
-function toggleClass(el, className) {
-    if (el.classList.contains(className)) {
-        el.classList.remove(className);
-    } else {
-        el.classList.add(className);
-    }
-}
+// function toggleClass(el, className) {
+//     if (el.classList.contains(className)) {
+//         el.classList.remove(className);
+//     } else {
+//         el.classList.add(className);
+//     }
+// }
 
 [...document.querySelectorAll('.item-1')].map(item => {
     item.addEventListener('click', event => {
-        toggleClass(event.currentTarget, 'active');
+        event.currentTarget.classList.toggle('active');
     });
 });
